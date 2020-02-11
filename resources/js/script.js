2dde3a4f20c8aca62430cdedb0c66f59e5ff44b0
Tamas Val <valtamas@hotmail.com>
2020-02-11 16:20:49 +0100

    Designed header section

---
+++ resources/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    const navigationIcon = $(".js--nav-icon i");
+    const navigationBarElements = $(".js--nav-list");
+  
+    $(navigationIcon).click(function() {
+
+  
+      if (navigationIcon.hasClass("ion-navicon-round")) {
+        navigationIcon.addClass("ion-close-round");
+        navigationIcon.removeClass("ion-navicon-round");
+        navigationBarElements.css("display", "flex");
+      } else {        
+        navigationIcon.addClass("ion-navicon-round");
+        navigationIcon.removeClass("ion-close-round");
+        navigationBarElements.css("display", "none");
+      }
+    });
+  });
+  
