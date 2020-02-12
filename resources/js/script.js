8f3488c67e97e7c6ae5c635508422d460cce5e48
vtominator <valtamas@hotmail.com>
2020-02-12 23:30:30 +0100

    Added navigation script

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,39 @@
 $(document).ready(function() {
-    const navigationIcon = $(".js--nav-icon i");
-    const navigationBarElements = $(".js--nav-list");
-  
-    $(navigationIcon).click(function() {
-
-  
-      if (navigationIcon.hasClass("ion-navicon-round")) {
-        navigationIcon.addClass("ion-close-round");
-        navigationIcon.removeClass("ion-navicon-round");
-        navigationBarElements.css("display", "grid");
-      } else {        
-        navigationIcon.addClass("ion-navicon-round");
-        navigationIcon.removeClass("ion-close-round");
-        navigationBarElements.css("display", "none");
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        let target = $(this.hash);
+        let position = 0;
+        target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          if (target[0].id == "about") {
+            position = target.offset().top;
+          } else {
+            position = target.offset().top + 215;
+          }
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: position
+            },
+            2000
+          );
+        }
       }
     });
+
+  $("#up").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 2000);
   });
-  
+});
